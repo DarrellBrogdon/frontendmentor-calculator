5289920c47447fb7ab96332ebbb47beaf3b83617
DarrellBrogdon <darrell@brogdon.net>
2021-10-08 18:05:57 -0600

    Refactor theme selection.
    This change moves the redefining of the CSS vars from script.js to
    style.css and then dynamically selects the theme by applying a class to
    the body.

--- script.js
+++ script.js
@@ -5,50 +5,10 @@ window.onload = function(){
     const themeSlider = document.getElementById('skin-selector');
 
     function themeSelect(value){
-        if(themeSlider.value == 1){
-            root.style.setProperty('--font-color', '#35352c');
-            root.style.setProperty('--display-background-color', '#ededed');
-            root.style.setProperty('--keypad-background-color', '#d1cccc');
-            root.style.setProperty('--special-operations-background-color', '#377f86');
-            root.style.setProperty('--special-operations-border-color', '#1b5f65');
-            root.style.setProperty('--special-operations-font-color', 'white');
-            root.style.setProperty('--equal-sign-background-color', '#ca5502');
-            root.style.setProperty('--equal-sign-border-color',  '#893901');
-            root.style.setProperty('--equal-sign-font-color', 'white');
-            root.style.setProperty('--background-color', '#e6e6e6');
-            root.style.setProperty('--button-bottom-color', '#a69d91');
-            root.style.setProperty('--button-color', '#e5e4e1');
-            root.style.setProperty('--button-font-color', '#444b5a');
-        }else if(themeSlider.value == 2){
-            root.style.setProperty('--font-color', '#ffe53d');
-            root.style.setProperty('--display-background-color', '#1d0934');
-            root.style.setProperty('--keypad-background-color', '#1d0934');
-            root.style.setProperty('--special-operations-background-color', ' #58077d');
-            root.style.setProperty('--special-operations-border-color', '#bc15f4');
-            root.style.setProperty('--special-operations-font-color', 'white');
-            root.style.setProperty('--equal-sign-background-color', '#00e0d1');
-            root.style.setProperty('--equal-sign-border-color',  '#6cf9f2');
-            root.style.setProperty('--equal-sign-font-color', '#1b2428');
-            root.style.setProperty('--background-color', '#160628');
-            root.style.setProperty('--button-bottom-color', '#871c9c');
-            root.style.setProperty('--button-color',  '#341c4f');
-            root.style.setProperty('--button-font-color', '#ffe53d');
-        }else{
-            root.style.setProperty('--font-color', 'white');
-            root.style.setProperty('--display-background-color', '#182034');
-            root.style.setProperty('--keypad-background-color', '#232c43');
-            root.style.setProperty('--special-operations-background-color', '#637097');
-            root.style.setProperty('--special-operations-border-color', '#404e72');
-            root.style.setProperty('--special-operations-font-color', 'white');
-            root.style.setProperty('--equal-sign-background-color', '#d03f2f');
-            root.style.setProperty('--equal-sign-border-color',  '#93261a');
-            root.style.setProperty('--equal-sign-font-color', 'white');
-            root.style.setProperty('--background-color', '#3a4764');
-            root.style.setProperty('--button-bottom-color', '#b4a597');
-            root.style.setProperty('--button-color', '#eae3dc');
-            root.style.setProperty('--button-font-color', '#444b5a');
-        }
+        document.querySelector('body').classList = []
+        document.querySelector('body').classList.add('theme-' + themeSlider.value)
     };
+
     //event listeners for theme change
         themeSlider.addEventListener('mousemove', themeSelect);
         themeSlider.addEventListener('change', themeSelect);
